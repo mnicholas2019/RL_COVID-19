88c40a294e560f23ad5ef95235c3baee8d653c8c
Matt's Linux Desktop <mnicholas@g.ucla.edu>
2020-05-26 14:20:18 -0700

    added comments

--- DQNAgent.py
+++ DQNAgent.py
@@ -13,24 +13,35 @@ class DQNAgent:
 		self.learning_rate = 0.001
 		self.model = self.build_model()
 
+
+	# initializes the model
 	def build_model(self):
 		return
 
+	# adds [state, action, reward, next_state, done] to memory deque
 	def memorize(self, state, action, reward, next_state, done):
 		return
 
-	def act(self, state)
+	# with probability epsilon returns a random action. With probability
+	# 1 - epsilon return the action with the minimum Q value (since our Q 
+	# is cost function). An action should be [city, action]. 
+	# city should be an integer 1-7 representing one of the cities.
+	# action should be 1 for water station and 2 for field hospital
+	def get_action(self, state)
 		return
 
+	# trains the neural network using batch_size instances. sample randomly from memory
 	def train_batch(self, state, action, reward, next_state, done, batch_size):
 		return
 
+	# train on specific instance
 	def train_individual(self, state, action, reward, next_state, done):
 		return
 
+	# save the model weights to 'name file path' so it can be loaded later
 	def save_model(self, name):
 		return
 
-	def save(self, name):
+	# load the model from name file path
+	def load(self, name):
 		return
-		

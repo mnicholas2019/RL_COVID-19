55ae5ab198bcdc76956c83d428471d196cb91a02
Matt's Linux Desktop <mnicholas@g.ucla.edu>
2020-05-29 14:19:57 -0700

    trying to pull

--- DQNAgent.py
+++ DQNAgent.py
@@ -11,57 +11,57 @@ import tensorflow as tf
 
 class DQNAgent:
 
-	def __init__(self, state_dimensions=58, action_dimensions=15, num_layers=4, num_parameters=75):
-		self.state_dimensions = state_dimensions
-		self.action_dimensions = action_dimensions
-		self.num_layers = num_layers
-		self.num_parameters = num_parameters
-		self.memory = deque() 
-		self.epsilon = 0.25 # exploration rate
-		self.gamma = 0.95 # discount future rewards rate
-		self.learning_rate = 0.001
-		self.model = self.build_model()
+    def __init__(self, state_dimensions=58, action_dimensions=15, num_layers=4, num_parameters=75):
+        self.state_dimensions = state_dimensions
+        self.action_dimensions = action_dimensions
+        self.num_layers = num_layers
+        self.num_parameters = num_parameters
+        self.memory = deque() 
+        self.epsilon = 0.25 # exploration rate
+        self.gamma = 0.95 # discount future rewards rate
+        self.learning_rate = 0.001
+        self.model = self.build_model()
 
 
-	# initializes the model
-	def build_model(self):
-                model = Sequential()
-                model.add(Dense(output_dim=self.num_parameters, input_dim=self.state_dimensions, activation='relu'))
-                for i in range(num_layers - 2):
-                        model.add(Dense(output_dim=self.num_parameters, activation='relu'))
-                model.add(Dense(output_dim=self.action_dimensions, activation='linear'))
-                model.compile(loss=self._huber_loss,
-                              optimizer=Adam(lr=self.learning_rate))
-                return model
+    # initializes the model
+    def build_model(self):
+        model = Sequential()
+        model.add(Dense(output_dim=self.num_parameters, input_dim=self.state_dimensions, activation='relu'))
+        for i in range(num_layers - 2):
+                model.add(Dense(output_dim=self.num_parameters, activation='relu'))
+        model.add(Dense(output_dim=self.action_dimensions, activation='linear'))
+        model.compile(loss=self._huber_loss,
+                      optimizer=Adam(lr=self.learning_rate))
+        return model
 
 
-	# adds [state, action, reward, next_state, done] to memory deque
-	def memorize(self, state, action, reward, next_state, done):
-                self.memory.append((state, action, reward, next_state, done))
+    # adds [state, action, reward, next_state, done] to memory deque
+    def memorize(self, state, action, reward, next_state, done):
+        self.memory.append((state, action, reward, next_state, done))
 
-	# with probability epsilon returns a random action. With probability
-	# 1 - epsilon return the action with the minimum Q value (since our Q 
-	# is cost function). An action should be [city, action]. 
-	# city should be an integer 1-7 representing one of the cities.
-	# action should be 1 for water station and 2 for field hospital
-	def get_action(self, state)
-                if np.random.rand() <= self.epsilon:
-                    return random.randrange(self.action_dimensions)
-                act_values = self.model.predict(Region.get_state())
-                return np.argmax(act_values[0])  # returns action
+    # with probability epsilon returns a random action. With probability
+    # 1 - epsilon return the action with the minimum Q value (since our Q 
+    # is cost function). An action should be [city, action]. 
+    # city should be an integer 1-7 representing one of the cities.
+    # action should be 1 for water station and 2 for field hospital
+    def get_action(self, state):
+        if np.random.rand() <= self.epsilon:
+            return random.randrange(self.action_dimensions)
+        act_values = self.model.predict(Region.get_state())
+        return np.argmax(act_values[0])  # returns action
 
-	# trains the neural network using batch_size instances. sample randomly from memory
-	def train_batch(self, state, action, reward, next_state, done, batch_size):
-		return
+    # trains the neural network using batch_size instances. sample randomly from memory
+    def train_batch(self, state, action, reward, next_state, done, batch_size):
+        return
 
-	# train on specific instance
-	def train_individual(self, state, action, reward, next_state, done):
-		return
+    # train on specific instance
+    def train_individual(self, state, action, reward, next_state, done):
+        return
 
-	# save the model weights to 'name file path' so it can be loaded later
-	def save_model(self, name):
-		return
+    # save the model weights to 'name file path' so it can be loaded later
+    def save_model(self, name):
+        return
 
-	# load the model from name file path
-	def load(self, name):
-		return
+    # load the model from name file path
+    def load(self, name):
+        return
--- main.py
+++ main.py
@@ -6,164 +6,164 @@ import sys
 
 
 def initialize_simulation():
-	covid19 = Disease()
+    covid19 = Disease()
 
-	cities = []
-	city1 = City(disease = covid19)
-	city2 = City(disease = covid19, population = 100000, area = 5, 
-				 hospital_beds = 200, num_infected = 2)
-	city3 = City(disease = covid19, population = 50000, area = 10, 
-				 hospital_beds = 100, num_infected = 20)
-	city4 = City(disease = covid19, population = 200000, area = 8, 
-				 hospital_beds = 400, num_infected = 3)
-	city5 = City(disease = covid19, population = 20000, area = 3, 
-				 hospital_beds = 40, num_infected = 10)
-	city6 = City(disease = covid19, population = 30000, area = 6, 
-				 hospital_beds = 60, num_infected = 15)
-	city7 = City(disease = covid19, population = 125000, area = 5, 
-				 hospital_beds = 250, num_infected = 1)
-	cities.append(city1)
-	cities.append(city2)
-	cities.append(city3)
-	cities.append(city4)
-	cities.append(city5)
-	cities.append(city6)
-	cities.append(city7)
+    cities = []
+    city1 = City(disease = covid19)
+    city2 = City(disease = covid19, population = 100000, area = 5, 
+                 hospital_beds = 200, num_infected = 2)
+    city3 = City(disease = covid19, population = 50000, area = 10, 
+                 hospital_beds = 100, num_infected = 20)
+    city4 = City(disease = covid19, population = 200000, area = 8, 
+                 hospital_beds = 400, num_infected = 3)
+    city5 = City(disease = covid19, population = 20000, area = 3, 
+                 hospital_beds = 40, num_infected = 10)
+    city6 = City(disease = covid19, population = 30000, area = 6, 
+                 hospital_beds = 60, num_infected = 15)
+    city7 = City(disease = covid19, population = 125000, area = 5, 
+                 hospital_beds = 250, num_infected = 1)
+    cities.append(city1)
+    cities.append(city2)
+    cities.append(city3)
+    cities.append(city4)
+    cities.append(city5)
+    cities.append(city6)
+    cities.append(city7)
 
-	region = Region(cities)
+    region = Region(cities)
 
-	return region
+    return region
 
 
 def play_step_days():
 
-	region = initialize_simulation()
-	day = 0
+    region = initialize_simulation()
+    day = 0
 
-	print("Day: ", day)
-	print(region.get_state())
+    print("Day: ", day)
+    print(region.get_state())
 
-	while 1:
-		day += 1
-		data = input("proceed to next day? (y/n)\n")
-		if data != 'n':
-			print("Day: ", day)
-			status = region.update()
-			print(region.get_state())
-			print("Cost: ", region.get_reward())
-		else:
-			break
-		if (status == 0):
-			break
+    while 1:
+        day += 1
+        data = input("proceed to next day? (y/n)\n")
+        if data != 'n':
+            print("Day: ", day)
+            status = region.update()
+            print(region.get_state())
+            print("Cost: ", region.get_reward())
+        else:
+            break
+        if (status == 0):
+            break
 
-	print("simulation over")
+    print("simulation over")
 
-	final_stats = region.get_final_stats()
+    final_stats = region.get_final_stats()
 
-	return final_stats
+    return final_stats
 
 def play_user_input():
 
-	region = initialize_simulation()
-	day = 0
+    region = initialize_simulation()
+    day = 0
 
-	print("Day: ", day)
-	print(region.get_state())
+    print("Day: ", day)
+    print(region.get_state())
 
-	while 1:
-		day += 1
-		action = input("water station (1), field hospital (2), nothing (3)")
-		print("action", action)
-		if action == '1' or action == '2':
-			city = input("which city? (1-7)")
-			region.take_action(city, action)
-		print("Day: ", day)
-		status = region.update()
-		print(region.get_state())
-		print("Cost: ", region.get_reward())
-		if (status == 0):
-			break
+    while 1:
+        day += 1
+        action = input("water station (1), field hospital (2), nothing (3)")
+        print("action", action)
+        if action == '1' or action == '2':
+            city = input("which city? (1-7)")
+            region.take_action(city, action)
+        print("Day: ", day)
+        status = region.update()
+        print(region.get_state())
+        print("Cost: ", region.get_reward())
+        if (status == 0):
+            break
 
-	print("simulation over")
+    print("simulation over")
 
-	final_stats = region.get_final_stats()
+    final_stats = region.get_final_stats()
 
 
-	return final_stats
+    return final_stats
 
 def run_sim_through():
-	region = initialize_simulation()
-	day = 0
+    region = initialize_simulation()
+    day = 0
 
-	print("Day: ", day)
-	print(region.get_state())
+    print("Day: ", day)
+    print(region.get_state())
 
-	while 1:
-		day += 1
-		print("Day: ", day)
-		status = region.update()
-		print(region.get_state())
-		print("Cost: ", region.get_reward())
-		if (status == 0):
-			break
+    while 1:
+        day += 1
+        print("Day: ", day)
+        status = region.update()
+        print(region.get_state())
+        print("Cost: ", region.get_reward())
+        if (status == 0):
+            break
 
-	print("simulation over")
+    print("simulation over")
 
-	final_stats = region.get_final_stats()
+    final_stats = region.get_final_stats()
 
 
-	return final_stats
+    return final_stats
 
 def train_agent(games):
 
-	weights_path = 'training_checkpoints/'
-	agent = DQNAgent()
-	# agent.load(some path)
+    weights_path = 'training_checkpoints/'
+    agent = DQNAgent()
+    # agent.load(some path)
 
-	game_counter = 0
-	simulation_results = []
+    game_counter = 0
+    simulation_results = []
 
-	while game_counter < games:
-		day = 0
-		region = initialize_simulation()
-		state = region.get_state()
-		done = 0
-		while 1:
-			state = region.get_state()
-			print("Day: ", day)
-			print(state)
-			action = agent.get_action(state)
+    while game_counter < games:
+        day = 0
+        region = initialize_simulation()
+        state = region.get_state()
+        done = 0
+        while 1:
+            state = region.get_state()
+            print("Day: ", day)
+            print(state)
+            action = agent.get_action(state)
 
-			region.take_action(action[0], action[1])
-			status = region.update()
-			#if done, set done to 1
-			new_state = region.get_state
+            region.take_action(action[0], action[1])
+            status = region.update()
+            #if done, set done to 1
+            new_state = region.get_state
 
-			reward = region.get_reward()
+            reward = region.get_reward()
 
-			agent.remember(state, action, reward, next_state, done)
-			#train with some probability for individual and group
+            agent.memorize(state, action, reward, next_state, done)
+            #train with some probability for individual and group
 
-			if status == 0:
-				break
+            if status == 0:
+                break
 
-		game_counter += 1
-		agent.save(weights_path + game_counter)
+        game_counter += 1
+        agent.save(weights_path + game_counter)
 
 
 
 
 if __name__ == "__main__":
-	
-	final_stats_sim = run_sim_through()
-	final_stats_game = play_user_input()
+    
+    final_stats_sim = run_sim_through()
+    final_stats_game = play_user_input()
 
-	print("Not infected: ", final_stats_sim[0])
-	print("Recovered: ", final_stats_sim[1])
-	print("Dead: ", final_stats_sim[2])
-	print("Cumulative days needing bed: ", final_stats_sim[3])
+    print("Not infected: ", final_stats_sim[0])
+    print("Recovered: ", final_stats_sim[1])
+    print("Dead: ", final_stats_sim[2])
+    print("Cumulative days needing bed: ", final_stats_sim[3])
 
-	print("Not infected: ", final_stats_game[0])
-	print("Recovered: ", final_stats_game[1])
-	print("Dead: ", final_stats_game[2])
-	print("Cumulative days needing bed: ", final_stats_game[3])
+    print("Not infected: ", final_stats_game[0])
+    print("Recovered: ", final_stats_game[1])
+    print("Dead: ", final_stats_game[2])
+    print("Cumulative days needing bed: ", final_stats_game[3])
